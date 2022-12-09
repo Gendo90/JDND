eea47d2767c248811f154d5c78570d95aabb8c16
Patrick Gendotti <patrick.gendotti@gmail.com>
2022-12-09 12:46:39 -0800

    feat: Add unit tests for the Cart Controller

---
+++ projects/P04-eCommerce Application/starter_code/src/test/java/com/example/demo/controllers/CartControllerUnitTest.java
@@ -0,0 +1,96 @@
+package com.example.demo.controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
+import com.example.demo.util.TestUtil;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CartControllerUnitTest {
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	@Mock
+	private CartRepository cartRepository;
+	
+	@Mock
+	private ItemRepository itemRepository;
+	
+	@InjectMocks
+	private CartController cartController;
+	
+	@Test
+	public void addToCartTest() {
+		// Cart attached to User, initially empty
+		User testUser = TestUtil.getTestUser();
+		
+		Item testItem = TestUtil.getFirstItem();
+		
+		ModifyCartRequest cartRequest = new ModifyCartRequest();
+		cartRequest.setUsername(testUser.getUsername());
+		cartRequest.setItemId(testItem.getId());
+		cartRequest.setQuantity(2);
+		
+		//Add mock outputs for given inputs here
+		when(userRepository.findByUsername(testUser.getUsername())).thenReturn(testUser);
+		when(itemRepository.findById(testItem.getId())).thenReturn(Optional.of(testItem));
+		
+		ResponseEntity<Cart> response = cartController.addTocart(cartRequest);
+		Cart resultCart = response.getBody();
+		
+		assertEquals(2, resultCart.getItems().size());
+		BigDecimal expectedCartTotal = testItem.getPrice().multiply(new BigDecimal(cartRequest.getQuantity()));
+		assertEquals(expectedCartTotal, resultCart.getTotal());
+	}
+	
+	@Test
+	public void removeFromCartTest() {
+		// Cart attached to User, initially empty
+		User testUser = TestUtil.getTestUser();
+		
+		Item testItem = TestUtil.getFirstItem();
+		
+		// Add existing cart items to cart
+		List<Item> existingCartItems = TestUtil.getItemList();
+		
+		Cart testCart = testUser.getCart();
+		existingCartItems.forEach(item -> testCart.addItem(item));
+		
+		ModifyCartRequest cartRequest = new ModifyCartRequest();
+		cartRequest.setUsername(testUser.getUsername());
+		cartRequest.setItemId(testItem.getId());
+		cartRequest.setQuantity(1);
+		
+		BigDecimal expectedCartTotal = testCart.getTotal().subtract(testItem.getPrice().multiply(new BigDecimal(cartRequest.getQuantity())));
+		
+		//Add mock outputs for given inputs here
+		when(userRepository.findByUsername(testUser.getUsername())).thenReturn(testUser);
+		when(itemRepository.findById(testItem.getId())).thenReturn(Optional.of(testItem));
+		
+		ResponseEntity<Cart> response = cartController.removeFromcart(cartRequest);
+		Cart resultCart = response.getBody();
+		
+		assertEquals(1, resultCart.getItems().size());
+		
+		assertEquals(expectedCartTotal, resultCart.getTotal());
+	}
+}
